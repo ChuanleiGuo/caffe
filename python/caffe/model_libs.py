913103f75510b9cade445c1162268d57e0d0867f
guo.chuanlei <chuanleiguo@gmail.com>
2018-01-26 15:03:19 +0800

    added ResNet50Body

--- python/caffe/model_libs.py
+++ python/caffe/model_libs.py
@@ -450,6 +450,52 @@ def VGGNetBody(net, from_layer, need_fc=True, fully_conv=False, reduced=False,
     return net
 
 
+def ResNet50Body(net, from_layer, use_pool5=True, use_dilation_conv5=False, **bn_param):
+    conv_prefix = ''
+    conv_postfix = ''
+    bn_prefix = 'bn_'
+    bn_postfix = ''
+    scale_prefix = 'scale_'
+    scale_postfix = ''
+    ConvBNLayer(net, from_layer, "conv1", use_bn=True, use_relu=True,
+        num_output=64, kernel_size=7, pad=3, stride=2,
+        conv_prefix=conv_prefix, conv_postfix=conv_postfix,
+        bn_prefix=bn_prefix, bn_postfix=bn_prefix,
+        scale_prefix=scale_prefix, scale_postfix=scale_postfix)
+
+    net.pool1 = L.Pooling(net.conv1, pool=P.Pooling.MAX, kernel_size=3, stride=2)
+
+    ResBody(net, "pool1", "2a", out2a=64, out2b=64, out2c=256, stride=1, use_branch1=True, **bn_param)
+    ResBody(net, "res2a", "2b", out2a=64, out2b=64, out2c=256, stride=1, use_branch1=False, **bn_param)
+    ResBody(net, 'res2b', '2c', out2a=64, out2b=64, out2c=256, stride=1, use_branch1=False, **bn_param)
+
+    ResBody(net, 'res2c', '3a', out2a=128, out2b=128, out2c=512, stride=2, use_branch1=True, **bn_param)
+    ResBody(net, "res3a", "3b", out2a=128, out2b=128, out2c=512, stride=1, use_branch1=False, **bn_param)
+    ResBody(net, "res3b", "3c", out2a=128, out2b=128, out2c=512, stride=1, use_branch1=False, **bn_param)
+    ResBody(net, "res3c", "3d", out2a=128, out2b=128, out2c=512, stride=1, use_branch1=False, **bn_param)
+
+    ResBody(net, "res3d", "4a", out2a=256, out2b=256, out2c=1024, stride=2, use_branch1=True, **bn_param)
+    ResBody(net, "res4a", "4b", out2a=256, out2b=256, out2c=1024, stride=1, use_branch1=False, **bn_param)
+    ResBody(net, "res4b", "4c", out2a=256, out2b=256, out2c=1024, stride=1, use_branch1=False, **bn_param)
+    ResBody(net, "res4c", "4d", out2a=256, out2b=256, out2c=1024, stride=1, use_branch1=False, **bn_param)
+    ResBody(net, "res4d", "4e", out2a=256, out2b=256, out2c=1024, stride=1, use_branch1=False, **bn_param)
+    ResBody(net, "res4e", "4f", out2a=256, out2b=256, out2c=1024, stride=1, use_branch1=False, **bn_param)
+
+    stride = 2
+    dilation = 1
+    if use_dilation_conv5:
+        stride = 1
+        dilation = 2
+
+    ResBody(net, "res4f", "5a", out2a=512, out2b=512, out2c=2048, stride=stride, use_branch1=True, dilation=dilation, **bn_param)
+    ResBody(net, "res5a", "5b", out2a=512, out2b=512, out2c=2048, stride=1, use_branch1=False, dilation=dilation, **bn_param)
+    ResBody(net, "res5b", "5c", out2a=512, out2b=512, out2c=2048, stride=1, use_branch1=False, dilation=dilation, **bn_param)
+
+    if use_pool5:
+        net.pool5 = L.Pooling(net.res5c, pool=P.Pooling.AVE, global_pooling=True)
+
+    return net
+
 def ResNet101Body(net, from_layer, use_pool5=True, use_dilation_conv5=False, **bn_param):
     conv_prefix = ''
     conv_postfix = ''
